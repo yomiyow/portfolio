e98f5a125cad68b4332b5b31ede25e0596b50214
Romeo Quinones <romeoquinones4@gmail.com>
2026-05-13 08:17:44 +0800

    style: resize  icon  and remove redundant class styling

--- app/components/TeckStackCard.tsx
+++ app/components/TeckStackCard.tsx
@@ -11,11 +11,10 @@ const TechStackCard = ({ src, title }: TeckStackCardProps) => {
       <div className="card bg-base-100 w-30 sm:w-[150px] shadow-sm">
         <div className="card-body items-center text-center">
           <Image
-            className="w-10 sm:lg-20 h-auto"
             src={src}
             alt={title}
-            width={80}
-            height={80}
+            width={40}
+            height={40}
           />
           <h2 className="text-sm sm:text-md">{title}</h2>
         </div>
