3db994466361faccad7cf8106c469489e27b414a
Romeo Quinones <romeoquinones4@gmail.com>
2026-05-22 09:37:30 +0800

    chore: add trailing slash to sitemap base URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://itsmeromeo.dev',
+      url: 'https://itsmeromeo.dev/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1.0,
