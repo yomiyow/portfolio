4b450b1b15e001209fb3c777bba45345c3192ba1
Romeo Quinones <romeoquinones4@gmail.com>
2026-05-25 14:30:50 +0800

    style: refactor project section layout

--- app/components/ProjectSection.tsx
+++ app/components/ProjectSection.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 const ProjectSection = () => {
   return (
     <section id="project" className="flex flex-col items-center min-h-screen gap-10">
-      <h2 className="text-4xl font-bold mt-24 mb-5 lg:mb-24">Projects</h2>
+      <h2 className="text-4xl font-bold mt-24 mb-5 lg:mb-10">Projects</h2>
       {/* Osas */}
-      <div className="flex flex-col xl:flex-row-reverse items-center justify-between gap-12 xl:gap-20 p-4 w-full max-w-7xl mx-auto">
-        <div className="mockup-browser border border-base-300 w-full xl:w-1/2 bg-base-200">
+      <div className="flex flex-col items-center justify-between gap-4 p-4 w-full max-w-6xl mx-auto">
+        <div className="mockup-browser border border-base-300 w-full">
           <div className="mockup-browser-toolbar">
             <div className="input">osascomplaints.dev</div>
           </div>
-          <div className="grid place-content-center border-t border-base-300 ">
-            <figure className="hover-gallery h-auto xl:h-[280px]">
+          <div className="grid place-content-center border-t border-base-300 xl:h-[540px]">
+            <figure className="hover-gallery">
               <Image src="/images/osas-1.png" alt="OSAS System Interface 1" width={2000} height={2000} />
               <Image src="/images/osas-2.png" alt="OSAS System Interface 2" width={2000} height={2000} />
               <Image src="/images/osas-3.png" alt="OSAS System Interface 3" width={2000} height={2000} />
@@ -19,8 +19,8 @@ const ProjectSection = () => {
           </div>
         </div>
 
-        <div className="flex flex-col gap-6 w-full xl:w-1/2 px-4 md:px-8">
-          <div className="space-y-4">
+        <div className="flex flex-col gap-6 w-full">
+          <div>
             <p className="text-left leading-8">
               A complaint management system for the school Office of Student Affairs that allows students to submit complaints online while enabling administrators to manage, track, and update complaint statuses.
 
@@ -63,12 +63,10 @@ const ProjectSection = () => {
         </div>
       </div>
       {/* Kubernetes */}
-      <div className="flex flex-col xl:flex-row-reverse items-center justify-between gap-12 xl:gap-20 p-4 w-full max-w-7xl mx-auto">
-        <div className="mockup-browser border border-base-300 w-full xl:w-1/2 bg-base-200">
-          <div className="mockup-browser-toolbar">
-          </div>
-          <div className="grid place-content-center border-t border-base-300">
-            <figure className="hover-gallery h-auto xl:h-[280px]">
+      <div className="flex flex-col items-center justify-between gap-4 p-4 w-full max-w-6xl mx-auto">
+        <div className="mockup-window border border-base-300 w-full">
+          <div className="grid place-content-center border-t border-base-300 xl:h-[540px]">
+            <figure className="hover-gallery">
               <Image src="/images/kubernetes/online-boutique-home.png" alt="Online Boutique Home Page" width={2000} height={2000} />
               <Image src="/images/kubernetes/online-boutique-cart.png" alt="Online Boutique Cart Page" width={2000} height={2000} />
               <Image src="/images/kubernetes/microservices-demo-github.png" alt="Microservices Demo GitHub" width={2000} height={2000} />
@@ -83,8 +81,8 @@ const ProjectSection = () => {
           </div>
         </div>
 
-        <div className="flex flex-col gap-6 w-full xl:w-1/2 px-4 md:px-8">
-          <div className="space-y-4">
+        <div className="flex flex-col gap-6 w-full">
+          <div>
             <p className="text-left leading-8">
               Deployed Google’s Online Boutique microservices application on a local kubernetes cluster using Docker Desktop, kubectl, and Helm, with Prometheus and Grafana for monitoring and metrics visualization.
             </p>
