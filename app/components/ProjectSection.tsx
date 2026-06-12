dcc8ed1e3caf64522d76a4a74d11053c8da7996e
Romeo Quinones <romeoquinones4@gmail.com>
2026-06-12 11:39:08 +0800

    fix: update repository link for the library management system project

--- app/components/ProjectSection.tsx
+++ app/components/ProjectSection.tsx
@@ -186,7 +186,7 @@ const ProjectSection = () => {
             <span className="badge badge-sm badge-primary badge-soft">MySQL</span>
             <span className="badge badge-sm badge-primary badge-soft">Flatlaf</span>
           </p>
-          <a href="https://github.com/yomiyow/flashcard-app#-demo" target="_blank" rel="noopener noreferrer" className="btn btn-square btn-ghost" title="View Repository">
+          <a href="https://github.com/yomiyow/library-management-system#-library-management-system" target="_blank" rel="noopener noreferrer" className="btn btn-square btn-ghost" title="View Repository">
             <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
             </svg>
